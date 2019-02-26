1dc1149a87ea07e82517920eadcdb298baf559e0
william821114 <william821114@gmail.com>
2019-02-26 16:38:53 +0800

    v1.0

---
+++ js/config.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// Make a copy of this file and save it as config.js (in the js directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+var SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+var API_KEY = '46257732';
+var SESSION_ID = '2_MX40NjI1NzczMn5-MTU0OTAzMTEzNzgzMH5DQW0wR3hrWWRsd2VGbTMvSXVCNnZHa05-UH4';
+var TOKEN = 'T1==cGFydG5lcl9pZD00NjI1NzczMiZzaWc9ZGJiN2U1YmJkOTY0NWJmMDFjY2RmZGQ0NWU5N2YxN2Q4NTJjOTEwNTpzZXNzaW9uX2lkPTJfTVg0ME5qSTFOemN6TW41LU1UVTBPVEF6TVRFek56Z3pNSDVEUVcwd1IzaHJXV1JzZDJWR2JUTXZTWFZDTm5aSGEwNS1VSDQmY3JlYXRlX3RpbWU9MTU1MTE1NDE3NCZub25jZT0wLjUxNDg3MTUxMzE0MTAxNzUmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU1Mzc0MjU3NyZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
